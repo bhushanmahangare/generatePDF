5dc529e9e068a2e44ff38a9bb0561413819b81f7
BHUSHAN MAHANGARE <bhushanmahangare1993@gmail.com>
2023-08-14 11:23:16 +0530

    Generate PDF file

---
+++ index.js
@@ -0,0 +1,50 @@
+const { createInvoice } = require("./createInvoice.js");
+
+
+const invoice = {
+  header: {
+    logo: 'plainsurf.png',
+    logoName: 'Plainsurf',
+    fromName: 'Plainsurf',
+    fromAddress: 'Pune 411046',
+    fromCity: 'Maharashtra'
+  },
+  footer: {
+    footerText: 'Payment is due within 15 days. Thank you (Plainsurf).'
+  },
+  shipping: {
+    name: "John Doe",
+    address: "1234 Main Street",
+    city: "San Francisco",
+    state: "CA",
+    country: "US",
+    postal_code: 94111
+  },
+  items: [
+    {
+      item: "TC 100",
+      description: "Toner Cartridge",
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: "USB_EXT",
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    },
+    {
+      item: "Laptop",
+      description: "Dell Laptop",
+      quantity: 3,
+      amount: 3000
+    }
+  ],
+  subtotal: 8000,
+  paid: 0,
+  invoice_nr: 1234
+};
+
+
+
+createInvoice(invoice, "invoice.pdf");
